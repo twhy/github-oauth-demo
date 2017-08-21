6bbd9986c26b98706eb4a28e8dd6326dd4d51107
Tom Wang <tom.wanghaiyi@gmail.com>
2017-08-21 18:40:16 +0800

    Fix request params

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/access_token', function(req, res) {
-  request.post('https://github.com/login/oauth/access_token', {
-    json: true,
-    body: JSON.stringify(req.body)
+  request.post({
+    url: 'https://github.com/login/oauth/access_token',
+    form: req.body,
+    headers: { Accept: 'application/json' }
   }, function(error, response, body) {
     if (error) res.json(error)
     res.json(body)
